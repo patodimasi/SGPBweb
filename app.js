d4678d3153a1dfe531d20d70128ad57a47f03e06
patodimasi <pdimasi@hotmail.com>
2019-05-06 11:37:04 -0300

    modificacion archivo app

--- app.js
+++ app.js
@@ -4,20 +4,24 @@ const express = require('express');
 const app = express();
 
 //settings defino configuracion de express
-app.set('port',3000)
-app.use('/public',express.static('public'));
+app.set('port',3000);
+app.use('/public', express.static('public'));
+//app.use(express.static(__dirname + '/views'));
+
 
 //routes
 app.get('/',(req,res)=>{
     console.log(req.url);
     console.log(app);
-    res.send('hello');
+    res.send('hello moto');
 })
 
 app.get('/patricia',(req,res)=>{
     console.log(req.url);
-    res.send('hola mundo');
+    res.send('hola');
 })
+
+
 //inicio el servidor
 app.listen(app.get('port'),()=>{
     console.log('server on port',app.get('port'));
