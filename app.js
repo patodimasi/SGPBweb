d3d7995b5a1ac4bcb55b7db37ceb8116c5923969
patodimasi <pdimasi@hotmail.com>
2019-05-06 10:41:12 -0300

    agregado de rutas en el archivo app

--- app.js
+++ app.js
@@ -5,10 +5,18 @@ const app = express();
 
 //settings defino configuracion de express
 app.set('port',3000)
+app.use('/public',express.static('public'));
 
 //routes
 app.get('/',(req,res)=>{
+    console.log(req.url);
+    console.log(app);
     res.send('hello');
+})
+
+app.get('/patricia',(req,res)=>{
+    console.log(req.url);
+    res.send('hola mundo');
 })
 //inicio el servidor
 app.listen(app.get('port'),()=>{
