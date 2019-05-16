025cbfd43af3a99851982af140307834c2466d03
patodimasi <pdimasi@hotmail.com>
2019-05-16 16:52:04 -0300

    modificacion archivo app

--- app.js
+++ app.js
@@ -1,27 +1,12 @@
-// Inicializo el servidor express
+var express = require('express');
+var app = express();
 
-const express = require('express');
-const app = express();
-
-//settings defino configuracion de express
+//permite direccionar a direcciones estaticas....todas las paginas que esten en la carpeta public
+app.use(express.static(__dirname + '/public/'));
 app.set('port',3000);
-app.use('/public', express.static('public'));
-//app.use(express.static(__dirname + '/views'));
-
-//routes
-app.get('/',(req,res)=>{
-    console.log(req.url);
-    console.log(app);
-    res.send('hello moto');
-})
+// respond with "hello world" when a GET request is made to the homepage
 
 
-app.get('/patricia',(req,res)=>{
-    console.log(req.url);
-    res.send('hola');
-})
-
-//inicio el servidor
 app.listen(app.get('port'),()=>{
     console.log('server on port',app.get('port'));
 })
