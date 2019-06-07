64540fc090f2a3e6692877dacefb3dd6a0a10f16
patodimasi <pdimasi@hotmail.com>
2019-06-07 13:15:11 -0300

    modificacion archivo principal y altap de prueba

--- app.js
+++ app.js
@@ -35,6 +35,34 @@ app.get('/patricia',(req,res)=>{
 
 });
 
+app.get('/modificar',(req,res)=>{
+
+    var modificar ={
+       
+        "data":[
+            {
+            
+                    "DT_RowId": "row_1",
+                    "first_name": "Tiger",
+                    "last_name": "Nixon",
+                    "position": "System Architect",
+                    "email": "t.nixon@datatables.net",
+                    "office": "Edinburgh",
+                    "extn": "5421",
+                    "age": "61",
+                    "salary": "320800",
+                    "start_date": "2011-04-25"
+                  
+           
+            }
+        ]
+    }
+       res.write(JSON.stringify(modificar));
+       return res.end();
+       
+
+});
+
 app.get('/buscarTodosp',(req,res)=>{
     console.log("GET MATERIALES");
      planos.find(function(err, plano){
