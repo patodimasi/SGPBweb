34121c072d250cadfbdd57694be3f583d9dcc72f
patodimasi <pdimasi@hotmail.com>
2019-12-23 16:48:40 -0300

    cambio de permisos

--- public/usuarios.js
+++ public/usuarios.js
@@ -108,7 +108,22 @@ $(document).ready(function(){
                 { title: "Apellido","className": "text-center","data": "USR_APELLIDO" },
                 { title: "Estado","className": "text-center","data": "USR_ESTADO" },   
                
-              
+                { title: "Permisos", 
+                "data": null,
+                "className": "text-center",
+                    'render': function (data, type, row) {
+                        
+                        return "<button id='"+JSON.stringify(data)+ "' data-toggle='tooltip'  title='Modificar permisos' onclick='Permiso(this)' class='fa fa-pencil'/>"
+                    }
+                },
+               
+                { title: "Baja", 
+                "data": null,
+                "className": "text-center",
+                    'render': function (data, type, row) {
+                        return "<button id='"+JSON.stringify(data)+ "' data-toggle='tooltip'  title='Baja usuario' onclick='Baja(this)' class='fa fa-trash-o'/>"
+                    }
+                },
                         
             ],
 
@@ -117,22 +132,150 @@ $(document).ready(function(){
             
         })
 
-       /* $('#tusuarios tbody').on('click', 'td.details-control', function () {
-            var tr = $(this).closest('tr');
-            var row = table.row( tr );
-     
-            if ( row.child.isShown() ) {
-                // This row is already open - close it
-                row.child.hide();
-                tr.removeClass('shown');
-            }
-            else {
-                // Open this row
-                row.child( format(row.data()) ).show();
-                tr.addClass('shown');
-            }
-        } );
-        */
     });
 });
+// ---------------------------------------------------------------------------------------------------------------------------------  
+//-------------------------Se guardan los permisos modificados
+function Aceptarmodif(item){
+  
+    var INGJ =  $('#PER_INGJ').is(":checked");
+    var INGS = $('#PER_INGS').is(":checked");
+    var CC = $("#PER_CC").is(":checked");
+    var P =   $("#PER_P").is(":checked");
+    var ADMIN = $("#PER_ADMIN").is(":checked");
+    var ROOT = $("#PER_ROOT").is(":checked");
+    $('#Aceptarmodifpu').attr('disabled', false);
 
+    var permisos = [INGJ,INGS,CC,P,ADMIN,ROOT];
+   
+    for(var i = 0;i < permisos.length;i++){
+        if((permisos[i] == true)){
+            permisos[i] = 'S'
+        }
+        else{
+            permisos[i] = 'N'
+        }
+    }
+
+    var logon =  $('#usuario').val();
+    console.log(permisos);
+
+    $.ajax({
+        method : "GET",
+        async:true,
+        url:"/modpermiso_usu",
+        dataType : 'json',
+        data:{logon,permisos},
+
+        success: function(res){
+            console.log(res);
+            var myModalmodifper = $('#myModalmodifper');
+            if(res == "OK"){
+                myModalmodifper.find('.modal-body').text('Los permisos del usuario se modificaron correctamente').attr('style', 'text-align: center','bold').css('font-weight','Bold');
+                   
+            }
+            else{
+                myModalmodifper.find('.modal-body').text('Error al modificar los permisos para el usuario').attr('style', 'text-align: center','bold').css('font-weight','Bold');
+            }
+            myModalmodifper.modal('show');
+        }
+    })
+    $('#myModalpermiso').modal('hide');  
+    $('#Cerrarper').click();
+}
+// ---------------------------------------------------------------------------------------------------------------------------------  
+// ------------------ Prohibe que se pueda modificar mas de un checkbox--------------------------------------------------------------
+$("input:checkbox").on('click', function() {
+    console.log("llega el checkkkkkkkkk")
+    var box = $(this);
+    if (box.is(":checked")) {
+      var group = "input:checkbox[name='" + box.attr("name") + "']";
+      $(group).prop("checked", false);
+      box.prop("checked", true);
+    } else {
+      box.prop("checked", false);
+    }
+  });
+
+// ---------------------------------------------------------------------------------------------------------------------------------  
+// ------------------ Modifica  los permisos de un usuario---------------------------------------------------------------------------- 
+$(document).ready(function(){
+    $("#modif_permiso").click(function(){
+    //habilito todos los checkbox
+        $("#PER_INGJ").attr("disabled", false);
+        $("#PER_INGS").attr("disabled", false);
+        $("#PER_CC").attr("disabled", false);
+        $("#PER_P").attr("disabled", false);
+        $("#PER_ADMIN").attr("disabled", false);
+        $("#PER_ROOT").attr("disabled", false);
+    })
+})
+// ---------------------------------------------------------------------------------------------------------------------------------  
+// ------------------ Muestra los permisos de un usuario----------------------------------------------------------------------------
+
+function Permiso(item){
+    infodp = JSON.parse($(item).attr("id"));
+ 
+    $('#myModalpermiso').modal();
+  //  $('#Aceptarmodifpu').attr('disabled', true);
+    $("#nombre").val(infodp.USR_NOMBRE);
+    $("#apellido").val(infodp.USR_APELLIDO);
+    $("#usuario").val(infodp.USR_LOGON);
+    
+    codigo = infodp.USR_CODIGO;
+    
+    //paso el codigo del usuario para que se busque en la tabla
+    $.ajax({
+        method : "GET",
+        async:true,
+        url:"/mostrar_usu",
+        dataType : 'json',
+        data:{codigo},
+        success: function(res){ 
+            
+            if(res[0].PER_INGJ == 'N'){
+               // $("#PER_INGJ").attr("disabled", true);
+                $("#PER_INGJ").attr('checked', false);
+            }                   
+         
+            if(res[0].PER_INGS == 'N'){
+              //  $("#PER_INGS").attr("disabled", true);
+                $("#PER_INGS").attr('checked', false);
+            }
+           
+            if(res[0].PER_CC == 'N'){
+                //$("#PER_CC").attr("disabled", true);
+                $("#PER_CC").attr('checked', false);
+            }
+
+            if(res[0].PER_P == 'N'){
+                //$("#PER_P").attr("disabled", true);
+                $("#PER_P").attr('checked', false);
+            }
+            
+            if(res[0].PER_ADMIN == 'N'){
+                //$("#PER_ADMIN").attr("disabled", true);
+                $("#PER_ADMIN").attr('checked', false);
+            }
+
+            if(res[0].PER_ROOT == 'N'){
+                //$("#PER_ROOT").attr("disabled", true);
+                $("#PER_ROOT").attr('checked', false);
+            }
+        }
+        
+    });
+    
+};
+
+$(document).ready(function(){
+    $("#Cerrarper").click(function(){
+    //habilito todos los checkbox
+        $("#PER_INGJ").attr("disabled", true);
+        $("#PER_INGS").attr("disabled", true);
+        $("#PER_CC").attr("disabled", true);
+        $("#PER_P").attr("disabled", true);
+        $("#PER_ADMIN").attr("disabled", true);
+        $("#PER_ROOT").attr("disabled", true);
+    })
+})
