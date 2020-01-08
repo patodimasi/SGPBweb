683dbe2522dd76ff7a8212be3b271c4cfc5fb9d0
patodimasi <pdimasi@hotmail.com>
2020-01-08 14:36:14 -0300

    correccion alta y permiso de usuario

--- public/usuarios.js
+++ public/usuarios.js
@@ -62,6 +62,8 @@ $(document).ready(function(){
     );
         
 });
+
+
 function desbloquear_estado(estado){
     var boton = null;
 
@@ -82,6 +84,7 @@ function Btnaltau(){
     $("#nombrealtau").val('');
     $("#apellidoaltau").val('');
     $("#logonusu").val('');
+    $('#PER_INGJ').prop('checked', true);
     $('#myModalaltausuario').modal();
 
 }
@@ -119,7 +122,6 @@ $(document).ready(function(){
                 "data": null,
                 "className": "text-center",
                     'render': function (data, type, row) {
-                        console.log(data.USR_ESTADO);
                         return "<button id='"+JSON.stringify(data._id)+ "' data-toggle='tooltip' ' " + desbloquear_estado(data.USR_ESTADO) +" ' title='Baja usuario' onclick='Baja(this)' class='fa fa-trash-o'/>"
                      
                     }
@@ -135,19 +137,18 @@ $(document).ready(function(){
     });
 });
 // ---------------------------------------------------------------------------------------------------------------------------------  
-//-------------------------Se guardan los permisos modificados
+//-------------------------Se guardan los permisos modificados--------------------------------------------------------
 function Aceptarmodif(item){
-  
-    var INGJ =  $('#PER_INGJ').is(":checked");
-    var INGS = $('#PER_INGS').is(":checked");
-    var CC = $("#PER_CC").is(":checked");
-    var P =   $("#PER_P").is(":checked");
-    var ADMIN = $("#PER_ADMIN").is(":checked");
-    var ROOT = $("#PER_ROOT").is(":checked");
-    $('#Aceptarmodifpu').attr('disabled', false);
+    console.log("Aceptarmodif");
+    var INGJ =  $('#P_PER_INGJ').is(":checked");
+    var INGS = $('#P_PER_INGS').is(":checked");
+    var CC = $("#P_PER_CC").is(":checked");
+    var P =   $("#P_PER_P").is(":checked");
+    var ADMIN = $("#P_PER_ADMIN").is(":checked");
+    var ROOT = $("#P_PER_ROOT").is(":checked");
 
     var permisos = [INGJ,INGS,CC,P,ADMIN,ROOT];
-   
+    console.log(permisos);
     for(var i = 0;i < permisos.length;i++){
         if((permisos[i] == true)){
             permisos[i] = 'S'
@@ -158,7 +159,7 @@ function Aceptarmodif(item){
     }
 
     var logon =  $('#usuariopermiso').val();
-    console.log(permisos);
+    console.log(logon);
 
     $.ajax({
         method : "GET",
@@ -179,13 +180,15 @@ function Aceptarmodif(item){
             }
             myModalmodifper.modal('show');
         }
+
     })
+    
     $('#myModalpermiso').modal('hide');  
     $('#Cerrarper').click();
 }
 // ---------------------------------------------------------------------------------------------------------------------------------  
 // ------------------ Prohibe que se pueda modificar mas de un checkbox--------------------------------------------------------------
-$("input:checkbox").on('click', function() {
+/*$("input:checkbox").on('click', function() {
     var box = $(this);
     if (box.is(":checked")) {
       var group = "input:checkbox[name='" + box.attr("name") + "']";
@@ -195,27 +198,35 @@ $("input:checkbox").on('click', function() {
       box.prop("checked", false);
     }
   });
-
+*/
 // ---------------------------------------------------------------------------------------------------------------------------------  
 // ------------------ Modifica  los permisos de un usuario---------------------------------------------------------------------------- 
 $(document).ready(function(){
     $("#modif_permiso").click(function(){
     //habilito todos los checkbox
-        $("#PER_INGJ").attr("disabled", false);
-        $("#PER_INGS").attr("disabled", false);
-        $("#PER_CC").attr("disabled", false);
-        $("#PER_P").attr("disabled", false);
-        $("#PER_ADMIN").attr("disabled", false);
-        $("#PER_ROOT").attr("disabled", false);
+        $('#Aceptarmodifpu').attr('disabled', false);
+        $("#P_PER_INGJ").attr("disabled", false);
+        $("#P_PER_INGS").attr("disabled", false);
+        $("#P_PER_CC").attr("disabled", false);
+        $("#P_PER_P").attr("disabled", false);
+        $("#P_PER_ADMIN").attr("disabled", false);
+        $("#P_PER_ROOT").attr("disabled", false);
+        
     })
 })
+
+
+
 // ---------------------------------------------------------------------------------------------------------------------------------  
 // ------------------ Muestra los permisos de un usuario----------------------------------------------------------------------------
 
 function Permiso(item){
+    $('#Aceptarmodifpu').attr('disabled', true);
+
     infodp = JSON.parse($(item).attr("id"));
  
-  /*  $('#myModalpermiso').modal();
+    $('#myModalpermiso').modal();
+
   
     $("#nombrepermiso").val(infodp.USR_NOMBRE);
     $("#apellidopermiso").val(infodp.USR_APELLIDO);
@@ -231,60 +242,51 @@ function Permiso(item){
         dataType : 'json',
         data:{codigo},
         success: function(res){ 
-            console.log("Esto son los permisos");
-            console.log(res)
-           
-            if(res[0].PER_INGJ == 'S'){
-                $("#PER_INGJ").attr('checked', true);
-                console.log("llega al ingj");
-            }                   
-            if(res[0].PER_INGS == 'S'){
-            
-                $("#PER_INGS").prop('checked', true);  
-                console.log("llega al INGS");
-            }
-            if(res[0].PER_CC == 'S'){
-                $("#PER_CC").attr('checked', true); 
-                console.log("llega al cc");
-            }
-            if(res[0].PER_P == 'S'){
-                $("#PER_P").attr('checked', true);  
-                console.log("llega al P");
-            }
-            if(res[0].PER_ADMIN == 'S'){
-                $("#PER_ADMIN").attr('checked', true);  
-                console.log("llega al admin"); 
-            }
-            if(res[0].PER_ROOT == 'S'){
-                $("#PER_ROOT").attr('checked', true);
-                console.log("llega al root");
-            }
+            console.log(res[0]);
+            console.log("-----------------------")
+            for( var prop in res[0] ) {
                
-            
+                if(res[0][prop] == 'S'){
+                    
+                    console.log("SI" + " " + prop);
+                  
+                    $("#P_" +  prop).prop('checked',true);
+                  
+                    console.log("-----------------------")
+                }
+                else{
+                    console.log("NO" + " " + prop);
+                    $("#P_" + prop).prop('checked',false);
+                }
+                
+            }
+          
         }
         
         
     });
-    */
+    
 };
 
 $(document).ready(function(){
    $("#Cerrarper").click(function(){
    
-        $("#PER_INGJ").attr("checked", false);
-        $("#PER_INGS").attr("checked", false);
-        $("#PER_CC").attr("checked", false);
-        $("#PER_P").attr("checked", false);
-        $("#PER_ADMIN").attr("checked", false);
-        $("#PER_ROOT").attr("checked", false);
+       /* $("#P_PER_INGJ").attr("checked", false);
+        $("#P_PER_INGS").attr("checked", false);
+        $("#P_PER_CC").attr("checked", false);
+        $("#P_PER_P").attr("checked", false);
+        $("#P_PER_ADMIN").attr("checked", false);
+        $("#P_PER_ROOT").attr("checked", false);
+        */
+     
+        $("#P_PER_INGJ").attr("disabled", true);
+        $("#P_PER_INGS").attr("disabled", true);
+        $("#P_PER_CC").attr("disabled", true);
+        $("#P_PER_P").attr("disabled", true);
+        $("#P_PER_ADMIN").attr("disabled", true);
+        $("#P_PER_ROOT").attr("disabled", true);
         
-        $("#PER_INGJ").attr("disabled", true);
-        $("#PER_INGS").attr("disabled", true);
-        $("#PER_CC").attr("disabled", true);
-        $("#PER_P").attr("disabled", true);
-        $("#PER_ADMIN").attr("disabled", true);
-        $("#PER_ROOT").attr("disabled", true);
-        
+       
     })
    
 });
@@ -322,7 +324,7 @@ function Consultausu(nombre, apellido){
                     "className": "text-center",
                         'render': function (data, type, row) {
                             
-                            return "<button id='"+JSON.stringify(data._id)+ "' data-toggle='tooltip'  title='Modificar permisos' onclick='Permiso(this)' class='fa fa-pencil'/>"
+                            return "<button id='"+JSON.stringify(data)+ "' data-toggle='tooltip'  " + desbloquear_estado(data.USR_ESTADO) +"  title='Modificar permisos' onclick='Permiso(this)' class='fa fa-pencil'/>"
                         }
                     },
                    
@@ -330,7 +332,7 @@ function Consultausu(nombre, apellido){
                     "data": null,
                     "className": "text-center",
                         'render': function (data, type, row) {
-                            return "<button id='"+JSON.stringify(data._id)+ "' data-toggle='tooltip'  title='Baja usuario' onclick='Baja(this)' class='fa fa-trash-o'/>"
+                            return "<button id='"+JSON.stringify(data._id)+ "' data-toggle='tooltip'  " + desbloquear_estado(data.USR_ESTADO) +" title='Baja usuario' onclick='Baja(this)' class='fa fa-trash-o'/>"
                         }
                     },
                     
@@ -376,6 +378,8 @@ $(document).ready(function(){
     
        if($("#nombrealtau").val()=="" || $("#apellidoaltau").val()=="" ){
             alert("Debe completar el nombre y el apellido");
+           
+
        }
        else
        {
@@ -401,37 +405,37 @@ $(document).ready(function(){
 function Aceptaraltausu(nombre, apellido){
     var estado_check;
     var logon;
-
+   
     $('#myModalaltausuario').modal('hide');  
    
     if($("#logonusu").val()=="")
     {
         alert("Debe generar un usuario");
+
     }
-    
-    logon = $("#logonusu").val();
-    $('input[type=checkbox]:checked').each(function() {
-       // console.log("Checkbox " + $(this).prop("id") +  " (" + $(this).val() + ") Seleccionado");
-        estado_check = $(this).prop("id");
-       
-    });
-    console.log(estado_check,nombre,apellido,logon);
-    $.ajax({
-        method : "GET",
-        async:true,
-        url:"/alta_usu",
-        dataType : 'json',
-        data:{estado_check,nombre,apellido,logon},
-     
-        success: function(res){ 
-         if(res == "OK"){
-            $("#myModalalta_usu").modal('show');
-         
-         }
-          
-        }
+    else{
+        logon = $("#logonusu").val();
+        $('input[type=radio]:checked').each(function() {
+            estado_check = $(this).prop("id");
+            console.log(estado_check);
+        });
+        console.log(estado_check,nombre,apellido,logon);
+        $.ajax({
+            method : "GET",
+            async:true,
+            url:"/alta_usu",
+            dataType : 'json',
+            data:{estado_check,nombre,apellido,logon},
         
-    })
-    
+            success: function(res){ 
+            if(res == "OK"){
+                $("#myModalalta_usu").modal('show');
+            
+            }
+            
+            }
+            
+        })
+    }
 };
  
